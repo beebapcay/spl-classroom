230e505f13326d143f1cbe2c69a5cf333436edc5
Phat Duong <beebapcay@gmail.com>
2021-11-04 21:25:02 +0700

    Refactor class card

--- src/components/ClassCard/index.js
+++ src/components/ClassCard/index.js
@@ -3,15 +3,12 @@ import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardActionArea from '@mui/material/CardActionArea';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import TrendingUpIcon from '@mui/icons-material/TrendingUp';
-import _ from 'lodash';
 import { backgrounds } from '../../configs/default';
+import _ from 'lodash';
 
 const ClassCard = ({ classItem, ...props }) => {
   const background = classItem.background || backgrounds[_.random(0, backgrounds.length - 1)];
@@ -54,21 +51,13 @@ const ClassCard = ({ classItem, ...props }) => {
       </div>
 
       <CardContent sx={{ minHeight: 135 }}>
-        <Typography gutterBottom variant="h6" component="p" sx={{ color: '#263248' }}>
+        <Typography gutterBottom variant="h5" component="p" sx={{ color: '#263248' }}>
           {classItem.name}
         </Typography>
         <Typography variant="body2" color="textSecondary" component="p">
           {classItem.section}
         </Typography>
       </CardContent>
-
-      <Divider />
-
-      <CardActions sx={{ justifyContent: 'end' }}>
-        <IconButton aria-label="class-trending-up">
-          <TrendingUpIcon />
-        </IconButton>
-      </CardActions>
     </Card>
   );
 };
